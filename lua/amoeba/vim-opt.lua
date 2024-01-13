c810ccbc4ae2d72f0cc2494f54bc3dc4fc8fb50e
Andres Perez <artofandresperez@gmail.com>
2024-01-13 08:51:58 -0800

    init nvim config as separate repo

---
+++ lua/amoeba/vim-opt.lua
@@ -0,0 +1,8 @@
+vim.wo.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
